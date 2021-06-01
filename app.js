7f9b044a13e60f100aa838fe7b063093bbc3d362
Denis <dkrishtaf98@gmail.com>
2021-06-01 22:30:31 +0300

    asd

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function init() {
 
   //Camera setup
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.set(0, 0, 0);
+  camera.position.set(0, 0, 5);
 
   const ambient = new THREE.AmbientLight(0x404040, 2);
   scene.add(ambient);
