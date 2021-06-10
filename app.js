fb0d645ba96eb9512ecf952593e15b9b4464d5be
Denis <dkrishtaf98@gmail.com>
2021-06-11 01:14:07 +0300

    cvb

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function init() {
 
   const fov = 35;
   const aspect = container.clientWidth / container.clientHeight;
-  const near = 0.1;
+  const near = 0.9;
   const far = 1000;
 
   //Camera setup
